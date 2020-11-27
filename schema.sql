2046d9310354b924cc8bcbbdabf70c6d2350d08e
lae2000 <lae2000@mail.ru>
2020-11-27 17:24:56 +0300

    Добавление связей в БД

--- schema.sql
+++ schema.sql
@@ -9,6 +9,12 @@ email VARCHAR(50) UNIQUE NOT NULL COLLATE 'utf8_general_ci',
 dt_reg_user DATE,
 user_pass VARCHAR(32) NOT NULL COLLATE 'utf8_general_ci');
 
+CREATE TABLE IF NOT EXISTS projects (
+id_pr INT AUTO_INCREMENT PRIMARY KEY,
+name_pr VARCHAR(200) NOT NULL COLLATE 'utf8_general_ci',
+id_user INT(9) NOT NULL,
+CONSTRAINT FK_user FOREIGN KEY (id_user) REFERENCES users(id) ON UPDATE RESTRICT ON DELETE RESTRICT);
+
 CREATE TABLE IF NOT EXISTS tasks (
 id INT AUTO_INCREMENT PRIMARY KEY,
 name_task VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
@@ -17,12 +23,9 @@ dt_end DATE NOT NULL,
 task_stat INT(1) NOT NULL DEFAULT 0,
 task_file VARCHAR(255) COLLATE 'utf8_general_ci',
 task_autor INT(9) NOT NULL,
-task_project INT(9) NOT NULL);
-
-CREATE TABLE IF NOT EXISTS projects (
-id_pr INT AUTO_INCREMENT PRIMARY KEY,
-name_pr VARCHAR(200) NOT NULL COLLATE 'utf8_general_ci',
-id_user INT(9) NOT NULL);
+task_project INT(9) NOT NULL,
+CONSTRAINT FK_autor FOREIGN KEY (task_autor) REFERENCES users(id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+CONSTRAINT FK_pr FOREIGN KEY (task_project) REFERENCES projects(id_pr) ON UPDATE RESTRICT ON DELETE RESTRICT);
 
 CREATE INDEX n_user on users (name_user);
 CREATE INDEX n_pr on projects (name_pr);
