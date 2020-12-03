ea649358c484079e8a3c15884fa4b9d9e3bbef1d
lae2000 <lae2000@mail.ru>
2020-12-03 17:56:46 +0300

    Внесение изменений в schema, index, создание запросов по задаче 2 из 4 модуля

--- schema.sql
+++ schema.sql
@@ -2,32 +2,36 @@ CREATE DATABASE IF NOT EXISTS doingsdone DEFAULT CHARACTER SET UTF8 DEFAULT COLL
 
 USE doingsdone;
 
+-- Создаем таблицу пользователей
 CREATE TABLE IF NOT EXISTS users (
 id INT AUTO_INCREMENT PRIMARY KEY,
-name_user VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+user_name VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
 email VARCHAR(50) UNIQUE NOT NULL COLLATE 'utf8_general_ci',
-dt_reg_user DATE,
+user_reg DATETIME DEFAULT NOW(),
 user_pass VARCHAR(32) NOT NULL COLLATE 'utf8_general_ci');
 
+-- Создаем таблицу проектов
 CREATE TABLE IF NOT EXISTS projects (
-id_pr INT AUTO_INCREMENT PRIMARY KEY,
-name_pr VARCHAR(200) NOT NULL COLLATE 'utf8_general_ci',
-id_user INT(9) NOT NULL,
-CONSTRAINT FK_user FOREIGN KEY (id_user) REFERENCES users(id) ON UPDATE RESTRICT ON DELETE RESTRICT);
+id INT AUTO_INCREMENT PRIMARY KEY,
+project_name VARCHAR(200) NOT NULL COLLATE 'utf8_general_ci',
+user_id INT(9) NOT NULL,
+CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE RESTRICT ON DELETE RESTRICT);
 
+-- Создаем таблицу задач
 CREATE TABLE IF NOT EXISTS tasks (
 id INT AUTO_INCREMENT PRIMARY KEY,
-name_task VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
-dt_create DATE NOT NULL,
-dt_end DATE NOT NULL,
-task_stat INT(1) NOT NULL DEFAULT 0,
+task_name VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+task_date_create DATETIME DEFAULT NOW(),
+task_date_end DATETIME DEFAULT NOW(),
+task_status INT(1) NOT NULL DEFAULT 0,
 task_file VARCHAR(255) COLLATE 'utf8_general_ci',
 task_autor INT(9) NOT NULL,
 task_project INT(9) NOT NULL,
-CONSTRAINT FK_autor FOREIGN KEY (task_autor) REFERENCES users(id) ON UPDATE RESTRICT ON DELETE RESTRICT,
-CONSTRAINT FK_pr FOREIGN KEY (task_project) REFERENCES projects(id_pr) ON UPDATE RESTRICT ON DELETE RESTRICT);
+CONSTRAINT FK_taskautor FOREIGN KEY (task_autor) REFERENCES users(id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+CONSTRAINT FK_taskproject FOREIGN KEY (task_project) REFERENCES projects(id) ON UPDATE RESTRICT ON DELETE RESTRICT);
 
-CREATE INDEX n_user on users (name_user);
-CREATE INDEX n_pr on projects (name_pr);
-CREATE INDEX t_stat on tasks (task_stat);
-CREATE INDEX n_task ON tasks (name_task);
+-- Создаем индексы к таблицам
+CREATE INDEX u_name on users (user_name);
+CREATE INDEX pr_name on projects (project_name);
+CREATE INDEX t_run on tasks (task_status);
+CREATE INDEX t_name ON tasks (task_name);
